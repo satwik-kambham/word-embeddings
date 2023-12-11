0f78c5b1e27735569566f73bb7746f14b8309062
Satwik Kambham <satwik.kambham@gmail.com>
2023-12-11 09:11:49 +0530

    Add configuration options and save vocabulary

--- word2vec/train.py
+++ word2vec/train.py
@@ -5,11 +5,32 @@ from datamodule import Word2VecDataModule
 
 
 def train():
-    dm = Word2VecDataModule(data_dir=".data")
+    L.seed_everything(42)
+
+    SUBSAMPLE_THRESHOLD = 1e-3
+    SAMPLE_CACHE_SIZE = 100000
+    WINDOW_SIZE = 10
+    NEGATIVE_PER_CONTEXT = 5
+    BATCH_SIZE = 1024
+    NUM_WORKERS = 2
+    EMBEDDING_DIM = 300
+
+    dm = Word2VecDataModule(
+        data_dir=".data",
+        subsample_threshold=SUBSAMPLE_THRESHOLD,
+        sampler_cache_size=SAMPLE_CACHE_SIZE,
+        window_size=WINDOW_SIZE,
+        negative_per_context=NEGATIVE_PER_CONTEXT,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+    )
     dm.prepare_data()
+
+    dm.train_vocab.save("vocab.json")
+
     model = EmbeddingModel(
         vocab_size=len(dm.train_vocab),
-        embedding_dim=128,
+        embedding_dim=EMBEDDING_DIM,
     )
 
     trainer = L.Trainer(
--- word2vec/vocab.py
+++ word2vec/vocab.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Vocab:
     def __init__(self):
         self.word2idx = {}
@@ -21,3 +24,17 @@ class Vocab:
 
     def decode(self, tokens):
         return [self.idx2word.get(idx, "<unk>") for idx in tokens]
+
+    def save(self, path):
+        with open(path, "w") as fp:
+            json.dump(self.word2idx, fp)
+
+    @classmethod
+    def load(cls, path):
+        with open(path, "r") as fp:
+            word2idx = json.load(fp)
+        vocab = cls()
+        vocab.word2idx = word2idx
+        vocab.idx2word = {idx: word for word, idx in word2idx.items()}
+        vocab.idx = len(word2idx)
+        return vocab
